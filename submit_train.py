0fbe10d4ad49f7fea99dd63620bf59129b177736
agata <mosinskaagata@gmail.com>
2024-09-15 17:12:11 +0200

    add further azure modifications

--- submit_train.py
+++ submit_train.py
@@ -1,4 +1,7 @@
 from azureml.core import Workspace, Experiment, ScriptRunConfig, Environment, Dataset
+import azureml._restclient.snapshots_client
+
+azureml._restclient.snapshots_client.SNAPSHOT_MAX_SIZE_BYTES = 2000000000
 
 # Step 1: Load the workspace from config.json located in the conf/ directory
 ws = Workspace.from_config(path="./conf/config.json")
@@ -17,8 +20,8 @@ datastore = ws.datastores['workspaceblobstore']  # Change this to your appropria
 train_dataset = Dataset.File.from_files(datastore.path('BraTS2024-BraTS-GLI-TrainingData/training_data1_v2'))
 val_dataset = Dataset.File.from_files(datastore.path('BraTS2024-BraTS-GLI-ValidationData/validation_data'))
 
-# Step 5: Create a Python environment for the experiment
-env = Environment.from_pip_requirements("my_env", "./requirements_azure.txt")
+# Step 5: Create an environment from the Conda YAML file
+env = Environment.from_conda_specification("my_custom_env", "./azureml-environment.yml")
 
 # Step 6: Set up the ScriptRunConfig to run your train.py script
 src = ScriptRunConfig(
@@ -27,7 +30,7 @@ src = ScriptRunConfig(
     compute_target=compute_target,
     environment=env,
     arguments=[
-        '--config', 'config.yaml',  # Add config file
+        '--config', './conf/config.yaml',  # Add config file
         '--train_data', train_dataset.as_mount(),  # Mount train data
         '--val_data', val_dataset.as_mount()  # Mount validation data
     ]
