6c4403059c5f90ef439ede006a80bcf8a5643ac4
Agata Mosinska <mosinskaagata@gmail.com>
2025-03-19 19:04:10 +0100

    add correlations between uncert and perf

--- src/uncertainty/correlate_uncertainty_and_performance.py
+++ src/uncertainty/correlate_uncertainty_and_performance.py
@@ -10,24 +10,29 @@ patient_uncertainties_path = "./outputs/uncertainties/patient_uncertainties.csv"
 patient_uncertainties = pd.read_csv(patient_uncertainties_path)
 
 performance_data = {
-    model_name: pd.read_csv(f"../models/performance/{model_name}/patient_metrics.csv")
+    model_name: pd.read_csv(f"../models/performance/{model_name}/composite_scores.csv")
     for model_name in models
 }
 
 correlation_results = []
 
 for model_name in models:
+    print("Model: ", model_name)
     patient_metrics_df = performance_data[model_name]
 
     for uncertainty_method in uncertainties:
+        print("Uncertainty method: ", uncertainty_method)
+
         for subregion in subregions:
+            print("Subregion: ", subregion)
             uncertainty_vals = patient_uncertainties.loc[
                 patient_uncertainties["Model"] == model_name,
                 f"{uncertainty_method} {subregion}",
             ]
-            dice_scores = patient_metrics_df[f"Dice {subregion}"]
+            scores = patient_metrics_df[f"{subregion}"]
 
-            correlation, _ = spearmanr(uncertainty_vals, dice_scores)
+            correlation, _ = spearmanr(uncertainty_vals, scores, nan_policy="raise")
+            print("Correlation: ", correlation)
 
             correlation_results.append(
                 {
@@ -39,7 +44,7 @@ for model_name in models:
             )
 
 correlation_df = pd.DataFrame(correlation_results)
-correlation_csv_path = "./outputs/correlations/spearman_correlations.csv"
+correlation_csv_path = "./outputs/correlations/spearman_correlations_composite_score.csv"
 os.makedirs(os.path.dirname(correlation_csv_path), exist_ok=True)
 correlation_df.to_csv(correlation_csv_path, index=False)
 
