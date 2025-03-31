7de5b4c32ad27bbe39548576b45339b13874a35f
Agata Mosinska <mosinskaagata@gmail.com>
2025-03-31 19:47:01 +0200

    fight with packages again

--- src/dataset/transforms.py
+++ src/dataset/transforms.py
@@ -1,5 +1,10 @@
 from monai import transforms
-
+import os
+import sys
+current_dir = os.path.dirname(os.path.realpath(__file__))
+repo_root = os.path.abspath(os.path.join(current_dir, ".."))
+if repo_root not in sys.path:
+    sys.path.insert(0, repo_root)
 
 def get_train_transforms(roi):
     """
