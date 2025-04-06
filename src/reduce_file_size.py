e1b5ef100633fd64e169abda684dd61b5f4f3c92
Agata Mosinska <mosinskaagata@gmail.com>
2025-04-06 15:30:43 +0200

    zip hospital images

---
+++ src/reduce_file_size.py
@@ -0,0 +1,12 @@
+import nibabel as nib
+import os 
+
+path_to_files = "/home/magata/data/braintumor_data/ARE/original"
+dir = os.listdir(path_to_files)
+
+for file in dir:
+    filepath = os.path.join(path_to_files, file)
+    nii = nib.load(filepath)
+    new_filepath = filepath + ".gz"
+    print(new_filepath)
+    nib.save(nii, new_filepath)
