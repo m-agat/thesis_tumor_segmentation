8cb3a9aa7edf44b5df27b5ae0b7afd70d9e391d7
Agata Mosinska <mosinskaagata@gmail.com>
2025-05-03 12:49:22 +0200

    remove entropy

--- src/uncertainty/test_time_dropout.py
+++ src/uncertainty/test_time_dropout.py
@@ -63,45 +63,6 @@ def ttd_variance(model, model_inferer, input_data, device, n_iterations=20, on_s
     return mean_output, variance_output
 
 
-
-def ttd_entropy(model, input_data, model_inferer, device, n_iterations=20):
-    """
-    Run test-time dropout to estimate uncertainty (entropy-based).
-
-    Args:
-        model: Trained model with dropout layers.
-        input_data: Input image tensor.
-        model_inferer: Inference function (e.g., sliding window inference).
-        n_iterations: Number of times to run inference with dropout active.
-
-    Returns:
-        mean_output: Averaged prediction over all iterations.
-        variance_output: Variance of predictions across iterations.
-    """
-    model.eval()  # Ensure model is in evaluation mode
-    enable_dropout(model)  # Enable dropout layers
-
-    all_outputs = []
-    with torch.no_grad(), torch.amp.autocast('cuda'):
-        for _ in tqdm(range(n_iterations), desc="Predicting with TTD.."):
-            output = torch.softmax(model_inferer(input_data), dim=1).to(device)
-            all_outputs.append(output.cpu().numpy())
-
-    # Convert to NumPy array for easier manipulation
-    all_outputs = np.array(all_outputs)
-
-    # Compute mean and variance across iterations
-    mean_output = np.mean(all_outputs, axis=0)
-    mean_output = np.squeeze(mean_output, axis=0)
-    mean_output = np.clip(mean_output, 1e-6, 1.0)
-
-    # Compute entropy: -p * log(p)
-    epsilon = 1e-6  # To avoid log(0)
-    entropy_output = -np.sum(mean_output * np.log(mean_output + epsilon), axis=0)
-
-    return mean_output, entropy_output
-
-
 def minmax_uncertainties(uncertainty_map):
     # scale: 0-1
     min_val = np.min(uncertainty_map)
